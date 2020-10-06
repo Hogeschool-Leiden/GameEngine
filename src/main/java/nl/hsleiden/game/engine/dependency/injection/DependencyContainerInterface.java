77cfc22f41d6cd67ee3e75e400ca35c40f433f51
Wim de Groot <34519486+degrootwim@users.noreply.github.com>
2020-10-06 15:15:23 +0200

    Delete DependencyContainerInterface.java

--- src/main/java/nl/hsleiden/game/engine/dependency/injection/DependencyContainerInterface.java
+++
@@ -1,8 +0,0 @@
-package nl.hsleiden.game.engine.dependency.injection;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface DependencyContainerInterface {
-    <Type, Dependency> void addTransient(Class<Type> type, Class<Dependency> dependency);
-    <Type> Object getDependency(Class<Type> typeClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException;
-}
