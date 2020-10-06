ece0d3ef8d05a8a35e05286e8d570d5b1686c888
Wim de Groot <34519486+degrootwim@users.noreply.github.com>
2020-10-06 15:17:43 +0200

    Update Helpers.java

--- src/main/java/nl/hsleiden/game/engine/utilities/Helpers.java
+++ src/main/java/nl/hsleiden/game/engine/utilities/Helpers.java
@@ -7,12 +7,12 @@ import nl.hsleiden.game.engine.physics.Vector;
 
 import java.util.Collection;
 
-public class Helpers {
-    public static BoundingBox toBoundingBox(Vector location, Dimension dimension) {
+public interface Helpers {
+    static BoundingBox toBoundingBox(Vector location, Dimension dimension) {
         return new BoundingBox(location, dimension);
     }
 
-    public static void drawBoundingBoxes(Collection<BoundingBox> boundingBoxes, Graphics graphics) {
+    static void drawBoundingBoxes(Collection<BoundingBox> boundingBoxes, Graphics graphics) {
         boundingBoxes.forEach(box -> graphics.drawRect(box.location, box.dimension, ARGBColor.RED));
     }
 }
